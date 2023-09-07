4b5b7676a110bd4e611f2bfc7f8038043543d918
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-09-07 16:22:00 +0100

    Fix (?) docs build

--- docs/static/hooks.py
+++ docs/static/hooks.py
@@ -1,13 +1,14 @@
+import tempfile
 from pathlib import Path
 
 import jsonschema2md
-import mkdocs.plugins
+import mkdocs
 import yaml
 from mkdocs.structure.files import File
 
+TEMPDIR = tempfile.TemporaryDirectory()
 
-# Bump priority to ensure files are moved before jupyter notebook conversion takes place
-@mkdocs.plugins.event_priority(50)
+
 def on_files(files: list, config: dict, **kwargs):
     """
     1. Link top-level files to mkdocs files.
@@ -19,6 +20,16 @@ def on_files(files: list, config: dict, **kwargs):
     files.append(_schema2md(config))
 
     return files
+
+
+@mkdocs.plugins.event_priority(-100)
+def on_post_build(**kwargs):
+    """After mkdocs has finished building the docs, remove the temporary directory of markdown files.
+
+    Args:
+        config (Config): mkdocs config dictionary (unused).
+    """
+    TEMPDIR.cleanup()
 
 
 def _new_file(path: str | Path, config: dict) -> File:
@@ -33,7 +44,7 @@ def _new_file(path: str | Path, config: dict) -> File:
 def _schema2md(config: dict):
     """Generate overview over configuration schema and add to mkdoc's files."""
     path_to_schema: Path = Path.cwd() / "schema.yaml"
-    path_to_md: Path = Path.cwd() / config["site_dir"] / "schema.md"
+    path_to_md: Path = Path(TEMPDIR.name) / "schema.md"
 
     parser = jsonschema2md.Parser()
     parser.tab_size = 4
@@ -41,11 +52,12 @@ def _schema2md(config: dict):
 
     lines = parser.parse_schema(schema)
     lines = _customise_markdown(lines)
+
     path_to_md.write_text("\n".join(lines))
 
     return File(
         path="schema.md",
-        src_dir=config["site_dir"],
+        src_dir=TEMPDIR.name,
         dest_dir=config["site_dir"],
         use_directory_urls=config["use_directory_urls"],
     )
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from pathlib import Path
 
-PROJECT_DIRECTORY = Path(".").absolute()
+PROJECT_DIRECTORY = Path.cwd().absolute()
 
 
 def remove_file(filepath: Path | str):
@@ -13,21 +13,22 @@ def remove_dir(dirpath: Path | str):
 
 
 if __name__ == "__main__":
-    if "{{ cookiecutter.create_author_file|lower }}" != "y":
+    if "{{ cookiecutter.create_author_file|lower }}" == "n":
         remove_file("AUTHORS")
 
-    if "{{ cookiecutter.command_line_interface|lower }}" != "y":
-        cli_file = Path("") / "{{ cookiecutter.project_slug }}" / "cli.py"
-        cli_docs = Path("") / "docs" / "api" / "cli.md"
+    if "{{ cookiecutter.command_line_interface|lower }}" == "n":
+        cli_file = Path() / "{{ cookiecutter.project_slug }}" / "cli.py"
+        cli_docs = Path() / "docs" / "api" / "cli.md"
         remove_file(cli_file)
         remove_file(cli_docs)
 
-    if "{{ cookiecutter.create_jupyter_notebook_directory|lower }}" != "y":
+    if "{{ cookiecutter.create_jupyter_notebook_directory|lower }}" == "n":
+        assert (PROJECT_DIRECTORY / "examples").exists()
         for hidden_file in (PROJECT_DIRECTORY / "examples").glob(".*"):
             hidden_file.unlink()
         remove_dir("examples")
 
-    if "{{ cookiecutter.create_docker_file|lower }}" != "y":
+    if "{{ cookiecutter.create_docker_file|lower }}" == "n":
         for file in [".dockerignore", "Dockerfile"]:
             remove_file(file)
 
