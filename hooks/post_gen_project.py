083d1d02a5bfba1db88bb7125195a9fcf4da832a
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-09-08 16:52:25 +0100

    Unlink example notebook if not using notebooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -24,10 +24,11 @@ if __name__ == "__main__":
         remove_file(cli_docs)
 
     if "{{ cookiecutter.create_jupyter_notebook_directory|lower }}" == "n":
-        assert (PROJECT_DIRECTORY / "examples").exists()
-        for hidden_file in (PROJECT_DIRECTORY / "examples").glob(".*"):
+        notebook_dir = Path("examples")
+        for hidden_file in (PROJECT_DIRECTORY / notebook_dir).glob(".*"):
             hidden_file.unlink()
-        remove_dir("examples")
+        remove_file(notebook_dir / "01_intro_to_{{cookiecutter.project_slug}}.ipynb")
+        remove_dir(notebook_dir)
 
     if "{{ cookiecutter.create_docker_file|lower }}" == "n":
         for file in [".dockerignore", "Dockerfile"]:
