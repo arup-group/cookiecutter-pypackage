51fefa6824fff0f3af5b320f825738f0762575ac
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-09-25 14:30:49 +0100

    Use conftest file

--- {{cookiecutter.repository_name}}/tests/test_core.py
+++ {{cookiecutter.repository_name}}/tests/test_core.py
@@ -1,25 +1,9 @@
-#!/usr/bin/env python
-
 """Tests for `{{ cookiecutter.module_name }}` package."""
 
-import pytest
-
 from {{ cookiecutter.module_name }} import core
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/arup-group/cookiecutter-pypackage')
 
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     print(core.__file__)
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-    #!/usr/bin/env python
