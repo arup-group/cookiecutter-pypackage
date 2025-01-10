ac20ddc0daf507d04bbc269760ced6c250d9985e
Docs <docs@dummy.bot.com>
2025-01-10 15:22:00 +0000

    Deployed d7343a5 to develop with MkDocs 1.6.1 and mike 2.1.3

--- develop/static/hooks.py
+++ develop/static/hooks.py
@@ -1,3 +1,5 @@
+"""Hooks for creating docs pages on-the-fly."""
+
 import tempfile
 from pathlib import Path
 
@@ -9,10 +11,19 @@ from mkdocs.structure.files import File
 TEMPDIR = tempfile.TemporaryDirectory()
 
 
-def on_files(files: list, config: dict, **kwargs):
-    """
+def on_files(files: list, config: dict, **kwargs) -> list:
+    """Update MKDocs pages.
+
     1. Link top-level files to mkdocs files.
     2. Clean up YAML schema
+
+    Args:
+        files (list): List of MKDocs project files.
+        config (dict): MKDocs project configuration.
+        **kwargs: necessary to capture inputs provided by MKDocs when it calls this function.
+
+    Returns:
+        list: Updated list of MKDocs project files.
     """
     files.append(_new_file("./CHANGELOG.md", config))
     files.append(_new_file("./CONTRIBUTING.md", config))
@@ -27,12 +38,21 @@ def on_post_build(**kwargs):
     """After mkdocs has finished building the docs, remove the temporary directory of markdown files.
 
     Args:
-        config (Config): mkdocs config dictionary (unused).
+        **kwargs (Config): mkdocs config dictionary (unused).
     """
     TEMPDIR.cleanup()
 
 
 def _new_file(path: str | Path, config: dict) -> File:
+    """Create an MKDocs File object.
+
+    Args:
+        path (str | Path): Path to file.
+        config (dict): MKDocs project configuration.
+
+    Returns:
+        File: File object.
+    """
     return File(
         path=path,
         src_dir=".",
@@ -41,15 +61,22 @@ def _new_file(path: str | Path, config: dict) -> File:
     )
 
 
-def _schema2md(config: dict):
-    """Generate overview over configuration schema and add to mkdoc's files."""
+def _schema2md(config: dict) -> File:
+    """Generate overview over configuration schema and add to mkdoc's files.
+
+    Args:
+        config (dict): MKDocs project configuration.
+
+    Returns:
+        File: File object.
+    """
     path_to_schema: Path = Path.cwd() / "schema.yaml"
     path_to_md: Path = Path(TEMPDIR.name) / "schema.md"
 
     parser = jsonschema2md.Parser()
     parser.tab_size = 4
     schema = yaml.safe_load(path_to_schema.read_text())
-
+    schema["properties"].pop("__prompts__")
     lines = parser.parse_schema(schema)
     lines = _customise_markdown(lines)
 
@@ -64,6 +91,14 @@ def _schema2md(config: dict):
 
 
 def _customise_markdown(lines: list) -> list:
+    """Clean up rendered Schema so that it doesn't read as a schema in the docs.
+
+    Args:
+        lines (list): rendered schema as a list of line strings
+
+    Returns:
+        list: Cleaned, rendered schema as a list of line strings
+    """
     # 1. Change headline
     assert lines[0] == "# JSON Schema\n\n"
     lines[0] = "# Configuration values\n\n"
